e50fd945827259e6040963e8a3e1b750f15ccba9
evg <evgkonst5002@gmail.com>
2024-06-06 09:05:16 +0300

    header

--- aura-parfums/src/Components/Header/Header.jsx
+++ aura-parfums/src/Components/Header/Header.jsx
@@ -28,22 +28,44 @@ const Header = () => {
 
     return (
         <header className={[styles.container, styles[getHeaderClass()]].join(" ")}>
-
+            
             <div className={styles.logo}>
-                <img src="https://img.freepik.com/free-vector/bird-colorful-logo-gradient-vector_343694-1365.jpg?size=338&ext=jpg&ga=GA1.1.632798143.1712966400&semt=ais" alt="" />
+                <Link to="/">
+                    <img src="https://img.freepik.com/free-vector/bird-colorful-logo-gradient-vector_343694-1365.jpg?size=338&ext=jpg&ga=GA1.1.632798143.1712966400&semt=ais" alt="" />
+                </Link>
             </div>
 
             <nav className={styles.navigation}>
-                <ul>
-                    <li><Link to="/">Home</Link></li>
-                    <li><Link to="/catalogs">Каталог</Link></li>
-                    <li><Link to="/brands">Бренды</Link></li>
-                    <li><Link to="/contacts">Контакты</Link></li>
-                    <li>Подобрать аромат</li>
-                    {/* <li><Link to="/homepage">Homepage</Link></li> */}
-                </ul>
-            </nav>
+                <div className={styles.nanContainer}>
+                    <div>
+                        <ul>
+                            <li><Link to="/catalogs">Каталог</Link></li>
+                            <li><Link to="/brands">Бренды</Link></li>
+                            <li><Link to="/contacts">Контакты</Link></li>
+                            <li>Подобрать аромат</li>
+                            {/* <li><Link to="/homepage">Homepage</Link></li> */}
+                        </ul>
+                    </div>
 
+                    <div className={styles.input}>
+                        <input type="text" />
+                    </div>
+
+                    <div className={styles.userLinks}>
+                        <a href="#">
+                            <img src={"Images/user.png"} alt="user" />
+                        </a>
+
+                        <a href="#">
+                            <img src={"Images/heart.png"} alt="heart" />
+                        </a>
+
+                        <a href="#">
+                            <img src={"Images/cart.png"} alt="cart" />
+                        </a>
+                    </div>
+                </div>
+            </nav>
         </header>
     );
 }
