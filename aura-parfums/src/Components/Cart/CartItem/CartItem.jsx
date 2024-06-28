76100152523d39a27538628d560469aed11861e2
evg <evgkonst5002@gmail.com>
2024-06-28 08:35:59 +0300

    ff

--- aura-parfums/src/Components/Cart/CartItem/CartItem.jsx
+++ aura-parfums/src/Components/Cart/CartItem/CartItem.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import styles from "./CartItem.module.scss";
 import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 const Counter = ({ size, count }) => {
     return (
@@ -23,7 +24,17 @@ const PriceBlock = ({ price }) => {
     );
 };
 
+
+
+
+
 const CartItem = ({ item }) => {
+
+    const removeItem = (event) => {
+        event.stopPropagation();
+        alert("hello");
+    }
+
     return (
         <Link to={`/product-page?${encodeURIComponent(item.productId)}`} >
             <div className={styles["cart-item"]}>
@@ -33,8 +44,13 @@ const CartItem = ({ item }) => {
 
                 <div className={styles["cart-item-info"]}>
                     <div className={styles["cart-item-info_top"]}>
-                        <span className={styles['cart-name']}>{item.name}</span>
-                        <span className={styles['cart-brand']}>{item.brand}</span>
+                        <div className={styles["product-info"]}>
+                            <span className={styles['cart-name']}>{item.name}</span>
+                            <span className={styles['cart-brand']}>{item.brand}</span>
+                        </div>
+                        <div className={styles["remove-btn"]}>
+                            <img onClick={removeItem} src="/Images/remove_btn.svg" alt="" />
+                        </div>
                     </div>
 
                     <div className={styles["cart-item-info_bottom"]}>
