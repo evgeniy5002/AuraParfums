c844bfac50cf0749a8118b23a3a4037aaef802bf
xeekes <legolas.200521@gmail.com>
2024-06-28 10:43:17 +0200

    e

--- aura-parfums/src/Components/OftenQuestions/OftenQuestions.jsx
+++ aura-parfums/src/Components/OftenQuestions/OftenQuestions.jsx
@@ -3,26 +3,26 @@ import styles from "./OftenQuestions.module.scss"
 
 const OftenQuestions = () => {
     return (
-        <div className={styles.OQcontainer}>
+        <div className={`${styles.OQcontainer} container`}>
             <div>
-            <h2>Найчастіші Запитання</h2>
-            <p>Якщо ви не знайшли відповіді на воє питання - пишіть нам на сайті або на наші сторінки у соцмережах</p>
-            <div className={styles.OQicons}>
-                <br /><br />
-            <a><i class="bi-instagram"></i></a>
-            <a><i class="bi-facebook"></i></a>
-            <a><i class="bi-telegram"></i></a>
+                <h2>Найчастіші Запитання</h2>
+                <p>Якщо ви не знайшли відповіді на воє питання - пишіть нам на сайті або на наші сторінки у соцмережах</p>
+                <div className={styles.OQicons}>
+                    <br /><br />
+                    <a><i class="bi-instagram"></i></a>
+                    <a><i class="bi-facebook"></i></a>
+                    <a><i class="bi-telegram"></i></a>
+                </div>
             </div>
-            </div>
-           <div className={styles.OQcontainerImgQuest}>
-            <div className={styles.OQcontainerQuestions}>
-                <div><button>Як здійснити замовлення?</button></div>
-                <div><button>Коли відправка замовлення?</button></div>
-                <div><button>Чи є післяплата?</button></div>
-                <div><button>Чи у вас оригінал?</button></div>
-                <div><button>Я зробив/зробила замовлення - що далі?</button></div>
-            </div>
-           <img src="Images/fcba80713bbdfad80ecb91b9bc193ff5.jpeg" alt=""/>
+            <div className={styles.OQcontainerImgQuest}>
+                <div className={styles.OQcontainerQuestions}>
+                    <div><button>Як здійснити замовлення?</button></div>
+                    <div><button>Коли відправка замовлення?</button></div>
+                    <div><button>Чи є післяплата?</button></div>
+                    <div><button>Чи у вас оригінал?</button></div>
+                    <div><button>Я зробив/зробила замовлення - що далі?</button></div>
+                </div>
+                <img src="Images/fcba80713bbdfad80ecb91b9bc193ff5.jpeg" alt=""/>
            </div>
         </div>
     )
